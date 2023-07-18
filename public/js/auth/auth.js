49a3cec63856afa8e76e68d77bb335674917f196
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-07-18 09:07:05 -0700

    Update auth.js

--- public/js/auth/auth.js
+++ public/js/auth/auth.js
@@ -3,6 +3,7 @@ export function login(){
     dox.getId('loginbtn').on('click', () => {
         let username = getState('username')
         let password = getState('password')
+        dox.getId('loginbtn').html('<span class="loading loading-lg  justify-center flex mx-auto loading-infinity"></span>
         pb.collection('users').authWithPassword(username, password).then((res) => {
            window.location.href = '#/'
         })
