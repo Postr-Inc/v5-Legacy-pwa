179b85bfdd8a29b47ff35d1b7ae4f8eb2392b25e
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-07-30 21:52:12 -0700

    Update feed.js

--- public/js/feed/feed.js
+++ public/js/feed/feed.js
@@ -98,10 +98,11 @@ export async function loadFeed(page = 1, perPage = 10) {
         }, 1000);
       });
 
-       if(!dox.querySelector('.loading-infinity').style.display == 'none'){
-          
-      dox.querySelector('.loading-infinity').style.display = 'none';
-       }
+       dox.awaitElement('.loading-infinity').then((res)=>{
+          if(!res.style.display == "none"){
+             res.style.display  = 'none' 
+          }
+       })
     });
 }
 
