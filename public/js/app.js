e45b9747d06052505ac44a92d40fd8d9953e38c0
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-08-04 15:19:09 -0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -10,4 +10,21 @@ if(!pb.authStore.isValid){
 }
 
 
- 
+ function appIsInstalled() {
+    if ('getInstalledRelatedApps' in navigator) {
+        navigator.getInstalledRelatedApps().then((installedApps) => {
+            console.log(installedApps);
+            if (installedApps.length > 0) {
+                console.log('PWA is installed.');
+            } else {
+                console.log('PWA is not installed.');
+            }
+        }).catch((error) => {
+            console.error('Error checking installed apps:', error);
+        });
+    } else {
+        console.log('The getInstalledRelatedApps method is not supported.');
+    }
+}
+
+appIsInstalled();
