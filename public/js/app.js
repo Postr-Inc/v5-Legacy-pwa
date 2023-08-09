ec5700d2b2d1b57d4c8c58c6b9e2b3a62f88615a
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-08-08 17:53:36 -0700

    Del -> func:appisinstalled
    getInstalledRelatedApps is not yet supported by most browsers so at the moment this function is obselete!

--- public/js/app.js
+++ public/js/app.js
@@ -10,24 +10,7 @@ if(!pb.authStore.isValid){
 }
 
 
-function appIsInstalled() {
-    if ('getInstalledRelatedApps' in navigator) {
-        navigator.getInstalledRelatedApps().then((installedApps) => {
-            console.log(installedApps);
-            if (installedApps.length > 0) {
-                console.log('PWA is installed.');
-            } else {
-                console.log('PWA is not installed.');
-            }
-        }).catch((error) => {
-            console.error('Error checking installed apps:', error);
-        });
-    } else {
-        console.log('The getInstalledRelatedApps method is not supported.');
-    }
-}
-
-appIsInstalled();
+ 
 
 window.onresize = (e) =>{
   window.innerHeight = "695"
