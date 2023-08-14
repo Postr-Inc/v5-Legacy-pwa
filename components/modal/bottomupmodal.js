607b8ca458e3708c1f247b96ec94465a6523e818
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-08-14 10:35:16 -0700

    init

---
+++ components/modal/bottomupmodal.js
@@ -0,0 +1,7 @@
+import { vhtml } from "vaderjs"
+
+const bottomupmodal = () => {
+    return vhtml`
+    <div class="fixed bottom-0 w-full h-1/2 bg-white rounded-t-3xl">
+    `
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+ 
+
 module.exports = {
-    content: [
-      './public/**/*.{html,js}',
-      './pages/**/*.{html,js}',
-      './components/**/*.{html,js}'
-    ],
+    content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
     },
-  
-    plugins: [require("daisyui")],
+    plugins: [require('daisyui')],
   }
