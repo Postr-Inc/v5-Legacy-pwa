648fafed7296d25f6288cb6ad3d824798de815b4
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-08-21 16:49:56 -0700

    Update routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import VaderRouter from "vaderjsRouter";
 import home from "../pages/Home.js";
 import Pocketbase from "pocketbase"
+import { register } from "../pages/Signup.js";
 const api = new Pocketbase('https://postr.pockethost.io')
 api.autoCancellation(false)
 export default api
-const app = new VaderRouter('/')
+const app = new VaderRouter('/home')
 app.use('/register')
-app.use('/')
-app.get('/', async (req, res) => {
-   res.render('#app', await home.render())
+app.use('/home')
+app.get('/home',  (req, res) => {
+   res.render('#app',   home.render())
+})
+app.on('/home',   (req, res) => {
+   res.render('#app',  home.render())
+})
+app.get('/register',   (req, res) => {
+   res.render('#app',   register.render())
+})
+app.on('/register', async (req, res) => {
+   res.render('#app',  register.render())
 })
 app.start()
  
