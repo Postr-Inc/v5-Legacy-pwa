b7823a9027a629101b63707a54f47c67f33ac4c4
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-08-23 07:36:11 -0500

    Update post.js
    Removed $templates from posts

--- src/components/post.js
+++ src/components/post.js
@@ -57,11 +57,9 @@ export const post = (props) => {
            }
             <svg
               onclick="likes_${props.id}()"
-              xmlns="http://www.w3.org/2000/svg" fill="
-              {likes.includes(api.authStore.model.id) ? '#F13B38' : 'none'}
-              " viewBox="0 0 24 24" stroke-width="1.5" stroke="
-              ${likes.includes(props.currentuser.id) ? '#F13B38' : 'currentColor'}
-              " class="w-6 h-6 cursor-pointer">
+              xmlns="http://www.w3.org/2000/svg" fill={likes.includes(api.authStore.model.id) ? '#F13B38' : 'none'}
+              viewBox="0 0 24 24" stroke-width="1.5" stroke={likes.includes(api.authStore.model.id) ? '#F13B38' : 'currentColor'}
+               class="w-6 h-6 cursor-pointer">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
             </svg>
              
