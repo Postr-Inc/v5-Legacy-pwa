f4e972f1db2e286037a5a41d295f549510fd11a3
Mini-Ware <codauknow@gmail.com>
2023-08-26 01:47:43 +0800

    added routing for settings

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import Home from './pages/home';
 import './styles/output.css'
 import Pocketbase from 'pocketbase'
 import { Profile } from './pages/profile';
+import { Settings } from './pages/settings'
 export const api = new Pocketbase('https://postr.pockethost.io')
 api.autoCancellation(false)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let app = new Router('/home')
+app.use('/settings')
 app.use('/profile')
 app.use('/home')
 app.get('/home', (req, res)=>{
@@ -18,6 +20,13 @@ app.get('/home', (req, res)=>{
 app.on('/home', (req, res)=>{
   console.log('home')
   root.render(<Home />)
+})
+app.get('/settings', (req, res)=>{
+  root.render(<Settings />)
+})
+app.on('/settings', (req, res)=>{
+  console.log('settings')
+  root.render(<Settings />)
 })
 app.on('/profile/:id', (req, res)=>{
   root.render(<Profile user={req.params.id} />)
