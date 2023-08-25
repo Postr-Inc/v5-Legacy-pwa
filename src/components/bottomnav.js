3abef126266577ac262598e1633997cc12831e64
Mini-Ware <codauknow@gmail.com>
2023-08-26 02:33:07 +0800

    fill account icon when in settings

--- src/components/bottomnav.js
+++ src/components/bottomnav.js
@@ -165,7 +165,7 @@ export const Bottomnav = () => {
               window.location.href = "#/profile/" + api.authStore.model.id;
             }}
           >
-            {window.location.hash === "#/profile/" + api.authStore.model.id ? (
+            {window.location.hash === "#/profile/" + api.authStore.model.id || window.location.hash === "#/settings" ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
